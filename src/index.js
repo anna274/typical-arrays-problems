571d22790b0954932094b2dea65e3f4d42b56f6a
anna274 <anna_rusakovich.01@mail.ru>
2020-03-09 20:42:39 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  return array.reduce((min, current) => Math.min(min, current), array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  return array.reduce((min, current) => Math.max(min, current), array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  return array.reduce((sum, current) => sum + current, 0)/array.length;
 }
